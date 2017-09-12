70aff824f5874f82430a1eea6cf67f5fa3012a0c
Oscar A. Ramirez <oscar.arturo.ramirez@gmail.com>
2017-09-12 17:28:19 -0400

    Seed file

--- seed.js
+++ seed.js
@@ -1,6 +1,5 @@
 const pkg = require('./package.json')
-const User = require('./server/db/models/user')
-const Product = require('./server/db/models/product')
+const {Product, Category, User} = require('./server/db/models/index')
 
 const Sequelize = require('sequelize');
 const db = new Sequelize(
@@ -13,18 +12,80 @@ const db = new Sequelize(
 
   ]
 
+  const products = [
+    {name: 'Shirt', description: 'Polo shirt', quantity: 10, price: 9.95},
+    {name: 'Pant', description: 'Kaki Pants', quantity: 100, price: 19.95}
+
+  ]
+
+  const categories = [
+    {name: 'Clothes'}
+  ]
+
   const seed = () =>
   Promise.all(users.map(user =>
     User.create(user))
   )
+  .then(() =>
+  Promise.all(categories.map(category =>
+    Category.create(category))
+  ))
+  .then(() =>
+  Promise.all(products.map(product =>
+    Product.create(Object.assign({}, product,
+      {categories: [
+        {name: 'Clothing'}
+      ]
+    }), {
+      include: [ Category ]
+    }))
+  )
+);
+  /* const seedUsers = () =>
+  Promise.all(users.map(user =>
+    User.create(user))}
+  )
+
+  const seedCategories = () =>
+  Promise.all(categories.map(category =>
+    Category.create(category))
+  )
+
+
+  var currentProduct = {};
+  const seedProducts = () =>
+  Promise.all(products.map(product =>
+    Product.create(product)
+    .then(product => {
+      currentProduct = product;
+      Category.findOne({
+        where: {
+          name: 'Clothes'
+        }
+      })
+      .then(category => currentProduct.setCategory(category))
+    }))
+  ) */
 
 const main = () => {
   console.log('Syncing db...')
   db.sync({ force: true })
+  .then(() => {
+    console.log('Seeding database...')
+    return seed()
+  })
+    /* .then(() => {
+      console.log('Seeding database...')
+      return seedUsers()
+    })
     .then(() => {
       console.log('Seeding database...')
-      return seed()
+      return seedCategories()
     })
+    .then(() => {
+      console.log('Seeding database...')
+      return seedProducts()
+    }) */
     .catch(err => {
       console.log('Error while seeding')
       console.log(err.stack)
