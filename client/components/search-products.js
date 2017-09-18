18e4ed3c91c8d8622183962b1effda1b68bff211
rendezvous3 <bojanj6m@gmail.com>
2017-09-18 17:34:03 -0400

    Changed file names to be consistant

--- client/components/search-products.js
+++ client/components/search-products.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import { writeInputValue } from '../store';
-import ProductList from './ProductList';
+import ProductList from './products-list';
 
 
 class SearchProducts extends Component {
